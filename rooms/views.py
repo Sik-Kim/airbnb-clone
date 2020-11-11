a88b09990934a3addc6b411476faffcb2e688e48
jeongsik <summering6@gmail.com>
2020-11-11 22:00:24 +0900

    airbnb clone updated

--- rooms/views.py
+++ rooms/views.py
@@ -1,3 +1,11 @@
+from datetime import datetime
 from django.shortcuts import render
+from . import models
 
-# Create your views here.
+# from django.http import HttpResponse
+
+
+def all_rooms(request):
+    all_rooms = models.Room.objects.all()
+
+    return render(request, "all_rooms.html", context={"room": all_rooms})
